04200788f39ccb1661d8252575c4ec2e0332c8f0
Hans Buchmann <hans.buchmann@fhnw.ch>
2014-09-15 18:29:32 +0200

    0-intro 1-setup

---
+++ devel/java/HelloWorld.java
@@ -0,0 +1,11 @@
+//---------------------
+//HelloWorld
+//(c) H.Buchmann FHNW 2014
+//---------------------
+class HelloWorld
+{
+ public static void main(String args[])
+ {
+  System.out.println(new java.util.Date());
+ }
+}
---
+++ devel/src/hello-world-cpp.cc
@@ -0,0 +1,11 @@
+/*----------------------
+  hello-world
+  (c) H.Buchmann FHNW 2014
+------------------------*/
+#include <iostream>
+
+int main(int argc,char** args)
+{
+ std::cout<<"Hello World "__DATE__"\n";
+ return 0;
+}
