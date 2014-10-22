712ed1ff465cc8dbfb5513737652e3e93d228b82
Hans Buchmann <hans.buchmann@fhnw.ch>
2014-10-22 12:04:00 +0200

    blink GPIO PIN 15

---
+++ devel/src/blink.sh
@@ -0,0 +1,15 @@
+#---------------------
+#blink.sh
+#(c) H.Buchmann FHNW 2014
+# copy into work
+#---------------------
+PIO=$((1<<15))
+PORT_ON=0x20200020
+PORT_OFF=0x2020002C
+while true
+do
+./hw-access wr ${PORT_ON} ${PIO}
+sleep 0.5
+./hw-access wr ${PORT_OFF} ${PIO}
+sleep 0.5
+done
