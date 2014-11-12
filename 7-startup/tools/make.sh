b04c3dd3a94b37aa6116e78e603a8194169e2346
edu <edu@fhnw.ch>
2014-11-12 13:45:27 +0100

    the steps for making u-boot.bin

--- 7-startup/tools/make.sh
+++ 7-startup/tools/make.sh
@@ -1,6 +1,9 @@
 #------------------
 #make.sh
 #(c) H.Buchmann FHNW 2014
+# step 1: distclean
+#      2: rpi_b_config
+#      3: u-boot.bin
 #------------------
 #we are in 7-startup
 . config/config.sh
