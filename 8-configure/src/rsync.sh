84229737ae5586df76791a45df08661baef42dab
edu <edu@fhnw.ch>
2014-11-26 15:26:25 +0100

    for raspberry

--- 8-configure/src/rsync.sh
+++ 8-configure/src/rsync.sh
@@ -6,15 +6,20 @@
 BUILD=build-rsync
 SRC=${PWD}/../resources/rsync-3.1.1
 PREFIX=${PWD}/work
+TARGET_ROOT=${PWD}/target-root
 #if [[ ! -d ${BUILD} ]]
 #  then mkdir ${BUILD}
 #fi
 
+export CC=${PWD}/tc/bin/armv6l-unknown-linux-gnueabihf-gcc
+export CFLAGS=--sysroot=${TARGET_ROOT}
 
 [[ ! -d ${BUILD} ]] && mkdir ${BUILD}
 echo "absolute src-path='${SRC}'s"
 
 cd ${BUILD}
-${SRC}/configure --prefix=${PREFIX}
+${SRC}/configure --prefix=${PREFIX} --host=armv6l-unknown-linux-gnueabihf
+#${SRC}/configure --help
+
 
  
