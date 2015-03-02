c4a616523d4bedc0bf8d80c69302bae65d159033
H.Buchmann <hans.buchmann@fhnw.ch>
2015-03-02 18:27:24 +0100

    the solution

--- 13-hardware/src/gpio.sh
+++ 13-hardware/src/gpio.sh
@@ -2,14 +2,23 @@
 #gpio.sh
 #(c) H.Buchmann FHNW 2015
 #${1} port
+#good ports:
+#   47: ACT_LED
+#   35: PWR_LED
 #-------------------
 GPIO=/sys/class/gpio
 pin=${1}
 echo "pin=${pin}"
-echo ${pin} > ${GPIO}/export
-echo out  > ${GPIO}/gpio${pin}/direction
+echo ${GPIO}/gpio${pin}
+[[ ! -d ${GPIO}/gpio${pin} ]] &&
+{
+ echo ${pin} > ${GPIO}/export;
+ echo out  > ${GPIO}/gpio${pin}/direction
+}
+
 echo 1    > ${GPIO}/gpio${pin}/value
 sleep 0.5
 echo 0    > ${GPIO}/gpio${pin}/value
 pin=$((pin+1))
+
 
