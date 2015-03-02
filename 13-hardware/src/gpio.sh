431cd959aab10dcd4fe6bb877df70efd0b8b5bce
H.Buchmann <hans.buchmann@fhnw.ch>
2015-03-02 18:28:27 +0100

    as exercise

--- 13-hardware/src/gpio.sh
+++ 13-hardware/src/gpio.sh
@@ -1,6 +1,7 @@
 #-------------------
 #gpio.sh
 #(c) H.Buchmann FHNW 2015
+#see kernel/Documentation/gpio.txt
 #${1} port
 #good ports:
 #   47: ACT_LED
@@ -8,17 +9,6 @@
 #-------------------
 GPIO=/sys/class/gpio
 pin=${1}
-echo "pin=${pin}"
-echo ${GPIO}/gpio${pin}
-[[ ! -d ${GPIO}/gpio${pin} ]] &&
-{
- echo ${pin} > ${GPIO}/export;
- echo out  > ${GPIO}/gpio${pin}/direction
-}
-
-echo 1    > ${GPIO}/gpio${pin}/value
-sleep 0.5
-echo 0    > ${GPIO}/gpio${pin}/value
-pin=$((pin+1))
+#your work
 
 
