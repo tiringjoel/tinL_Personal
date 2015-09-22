39178c9bc3aaf02964c27c045ad3e92827610411
H.Buchmann <hans.buchmann@fhnw.ch>
2015-09-22 11:12:02 +0200

    2-unix-use

---
+++ 2-unix-use/src/hello-world-c.c
@@ -0,0 +1,10 @@
+/*----------------------
+  hello-world
+  (c) H.Buchmann FHNW 2014
+------------------------*/
+#include <stdio.h>
+int main(int argc,char** args)
+{
+ printf("Hello World "__DATE__"\n");
+ return 0;
+}
