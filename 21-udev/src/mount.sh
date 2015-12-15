b7e7f91af672f07763e041917b0d5a30ad1534ee
H.Buchmann <hans.buchmann@fhnw.ch>
2015-12-15 14:08:25 +0100

    rules  not yet working

--- 21-udev/src/mount.sh
+++ 21-udev/src/mount.sh
@@ -6,10 +6,16 @@
 #--------------------
 BASENAME=$(dirname $(dirname ${0}))
 TARGET_ROOT=${BASENAME}/target-root
-[[ ! -d ${TARGET_ROOT} ]] && mkdir ${TARGET_ROOT} 
+echo "mount/link DEVNAME=${1} ID=${2}" >>/tmp/callback.log 
+echo "ls /run/media/buchmann/">>/tmp/callback.log
+ls /run/media/buchmann/>>/tmp/callback.log 
+#--------------- uncomment for mount
+#[[ ! -d ${TARGET_ROOT} ]] && mkdir ${TARGET_ROOT}
 echo "mount ${@} ${TARGET_ROOT}">>/tmp/callback.log 
 umount ${1} 2>>/tmp/callback.log 
 echo "umount error ${?}">>/tmp/callback.log 
-mount ${1} ${TARGET_ROOT} 2>>/tmp/callback.log
-echo "mount error ${?}">>/tmp/callback.log  
-
+#mount ${1} ${TARGET_ROOT} 2>>/tmp/callback.log
+gvfs-mount -d ${1} ${TARGET_ROOT} 2>>/tmp/callback.log
+echo "mount error ${?}">>/tmp/callback.log 
+echo "------------- ${TARGET_ROOT}">>/tmp/callback.log  
+ls ${TARGET_ROOT} >>/tmp/callback.log 
