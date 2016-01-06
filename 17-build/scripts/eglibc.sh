1af92f1bff822bfa0ca92823486d3526287704bc
H.Buchmann <hans.buchmann@fhnw.ch>
2016-01-06 16:10:19 +0100

    typos

--- 17-build/scripts/eglibc.sh
+++ 17-build/scripts/eglibc.sh
@@ -12,17 +12,17 @@ cd eglibc
 cp ${CONFIG}/option-groups.config .
 #${EGLIBC_SRC}/libc/configure --help
 DESTDIR=${SYSROOT} \
-${EGLIBC_SRC}/libc/configure \
-    --prefix=/usr \
-    --host=${TARGET} \mak
-    --build=x86_64-unknown-linux-gnu \
-    --with-headers=${SYSROOT}/usr/include \
-    --disable-profile --without-gd --without-cvs --enable-add-ons\
-    --enable-hacker-mode\
-    --with-elf \
-    --enable-kernel=2.6.20 \
-    --disable-sanity-checks
-
-#    --enable-rpc\
-${MAKE} 
+#${EGLIBC_SRC}/libc/configure \
+#    --prefix=/usr \
+#    --host=${TARGET} \
+#    --build=x86_64-unknown-linux-gnu \
+#    --with-headers=${SYSROOT}/usr/include \
+#    --disable-profile --without-gd --without-cvs --enable-add-ons\
+#    --enable-hacker-mode\
+#    --with-elf \
+#    --enable-kernel=2.6.20 \
+#    --disable-sanity-checks
+#
+##    --enable-rpc\
+#${MAKE} 
 ${MAKE} install install_root=${SYSROOT}
