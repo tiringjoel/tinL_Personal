c6bf222beeb02ab03b5d3a50e1585269a3805be9
H.Buchmann <hans.buchmann@fhnw.ch>
2016-11-01 12:01:58 +0100

    5-kernel  all scripts executable

--- 5-kernel/config/config.sh
+++ 5-kernel/config/config.sh
@@ -1,11 +1,14 @@
 #---------------------
 #config.sh
 #(c) H.Buchmann FHNW 2014
+
 #---------------------
 #we are in 5-kernel
 PREFIX=arm-linux-gnueabihf-
 TC=${PWD}/tc/bin/
 CROSS_COMPILE=${TC}/${PREFIX}
+
+#----------------------- adapt for your needs
 LINUX_SRC=${PWD}/../resources/beaglebone-black/linux
 BUILD=$(pwd)/build
 MODULES=$(pwd)/modules
--- 5-kernel/tools/kernel.sh
+++ 5-kernel/tools/kernel.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #---------------------
 #kernel.sh
 #(c) H.Buchmann FHNW 2015
