dee59517b92f9c081a15ee360f21af465a9b33b8
gitolite tester <tester@example.com>
2016-12-14 10:34:21 +0100

    rsync.sh  --prefix

--- 17-build/host/rsync.sh
+++ 17-build/host/rsync.sh
@@ -7,6 +7,7 @@ BUILD_HOME=${PWD}
 
 #where are the things use absolute pathnames
 RSYNC_SRC=${PWD}/../../resources/rsync-3.1.2
-
-${RSYNC_SRC}/configure --help
+PREFIX=${PWD}
+cd rsync-build
+${RSYNC_SRC}/configure --prefix=${PREFIX}
 
