dae153cb4fd74df99f81495683b6b5a4ba2e9555
gitolite tester <tester@example.com>
2016-12-14 10:44:27 +0100

    rsync.sh  make install

--- 17-build/host/rsync.sh
+++ 17-build/host/rsync.sh
@@ -10,5 +10,6 @@ RSYNC_SRC=${PWD}/../../resources/rsync-3.1.2
 PREFIX=${PWD}
 MAKE="make -j8"
 cd rsync-build
-${RSYNC_SRC}/configure --prefix=${PREFIX}
-${MAKE}
+#${RSYNC_SRC}/configure --prefix=${PREFIX}
+#${MAKE}
+${MAKE} install
