ee1bd0de5d448a11fa89ad619543ea7ce3892413
gitolite tester <tester@example.com>
2016-12-14 10:25:45 +0100

    rsync.sh  pathnames  configure --help

---
+++ 17-build/host/rsync.sh
@@ -0,0 +1,12 @@
+#--------------------
+#rsync.sh
+#(c) H.Buchmann FHNW 2016
+# example
+#--------------------
+BUILD_HOME=${PWD}
+
+#where are the things use absolute pathnames
+RSYNC_SRC=${PWD}/../../resources/rsync-3.1.2
+
+${RSYNC_SRC}/configure --help
+
