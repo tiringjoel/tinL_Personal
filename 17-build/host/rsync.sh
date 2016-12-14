3c0283d1b2036c6f75f1a513e611c7252552a8b2
gitolite tester <tester@example.com>
2016-12-14 10:45:02 +0100

    rsync.sh  empty again

--- 17-build/host/rsync.sh
+++ 17-build/host/rsync.sh
@@ -3,13 +3,3 @@
 #(c) H.Buchmann FHNW 2016
 # example
 #--------------------
-BUILD_HOME=${PWD}
-
-#where are the things use absolute pathnames
-RSYNC_SRC=${PWD}/../../resources/rsync-3.1.2
-PREFIX=${PWD}
-MAKE="make -j8"
-cd rsync-build
-#${RSYNC_SRC}/configure --prefix=${PREFIX}
-#${MAKE}
-${MAKE} install
