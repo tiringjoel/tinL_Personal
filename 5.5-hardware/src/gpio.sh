70c718b3b18d795c1c4aa97f40235c19eced0089
gitolite tester <tester@example.com>
2016-12-20 12:07:59 +0100

    5.5-hardware

--- 5.5-hardware/src/gpio.sh
+++ 5.5-hardware/src/gpio.sh
@@ -1,15 +1,18 @@
 #-------------------
 #gpio.sh
-#(c) H.Buchmann FHNW 2015
-#see kernel/Documentation/gpio.txt
+#(c) H.Buchmann FHNW 2016
+#see: kernel/Documentation/gpio/*
+#     doc/BBB_SRM.pdf p67
 #${1} port
 #good ports:
 # USR0,1,2,3
 # GPIO1 pin  21,22,23,24
 # GPIO1 Base 32
-# without LED support in kernel
+# pin#=base+pin
+# kernel without LED support:
 # Device Drivers
 #  LED Support
+
 #-------------------
 GPIO=/sys/class/gpio
 pin=${1}
