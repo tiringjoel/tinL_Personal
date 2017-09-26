f58c1967b151b59924874a595dddee99b25c6827
H.Buchmann <hans.buchmann@fhnw.ch>
2017-09-26 16:16:13 +0200

    for bbb wireless

--- 17-build/tools/create-toolchain.sh
+++ 17-build/tools/create-toolchain.sh
@@ -3,6 +3,7 @@
 #create-toolchain.sh
 #(c) H.Buchmann 2016
 #--------------------------
-#we are in repo
-TOOLCHAIN=gcc-6.2.0-arm-64bit
+#we are in 17-build
+find tc/ -executable -type f -exec strip '{}' \;
+TOOLCHAIN=gcc-7.2.0-arm-64bit
 tar --transform "s,^.,${TOOLCHAIN}," -czvf ${TOOLCHAIN}.tar.gz -C tc .
