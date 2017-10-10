3257cb91522279fefbee24d706c179e157107647
H.Buchmann <hans.buchmann@fhnw.ch>
2017-10-10 16:19:07 +0200

    creating sd-image

--- 17-build/tools/create-sd-image.sh
+++ 17-build/tools/create-sd-image.sh
@@ -26,46 +26,33 @@ ROOTFS_SIZE_SECT=$(((ROOTFS_SIZE_MB)<<11))
 BOOTFS_POS_SECT=${PARTTABLE_SIZE_SECT}
 ROOTFS_POS_SECT=$((BOOTFS_POS_SECT+BOOTFS_SIZE_SECT))
 
-dd if=/dev/urandom of=${IMG} count=${PARTTABLE_SIZE_SECT}
-dd if=/dev/urandom of=${BOOTFS} count=${BOOTFS_SIZE_SECT}
-dd if=/dev/urandom of=${ROOTFS} count=${ROOTFS_SIZE_SECT}
-mkfs.vfat ${BOOTFS}
-
-mkfs.ext4 ${ROOTFS}
-
-
-dd if=${BOOTFS} of=${IMG} seek=${BOOTFS_POS_SECT}
-dd if=${ROOTFS} of=${IMG} seek=${ROOTFS_POS_SECT}
-cat<<EOF | sfdisk ${IMG}
-boot :   start=${BOOTFS_POS_SECT} size=      ${BOOTFS_SIZE_MB}MiB, type=b, bootable
-rootfs : start=${ROOTFS_POS_SECT} size=      ${ROOTFS_SIZE_MB}MiB, type=83
-EOF
-
-
-#dd if=/dev/urandom of=${IMG} count=$(((SIZ_MB+17)<<11))
+#dd if=/dev/urandom of=${IMG} count=${PARTTABLE_SIZE_SECT}
+#dd if=/dev/urandom of=${BOOTFS} count=${BOOTFS_SIZE_SECT}
+#dd if=/dev/urandom of=${ROOTFS} count=${ROOTFS_SIZE_SECT}
 #
-
-#mkfs.vfat -C
- 
-#PART1=${IMG}1
-#PART2=${IMG}2
-#START1=$(partx -g -oSTART ${PART1} ${IMG})
-#START2=$(partx -g -oSTART ${PART2} ${IMG})
-##LOOP=$(sudo losetup -f)
-
-#sudo losetup -o $((START1*512)) ${LOOP} ${IMG}
-#sudo mkfs.vfat -nBOOT ${LOOP}
-#sudo mount ${LOOP} image/mount
-#sudo cp -v build/kernel/arch/arm/boot/zImage image/mount
-#sudo cp -v build/kernel/arch/arm/boot/dts/am335x-boneblack.dtb image/mount
-#sudo cp -v build/u-boot/MLO image/mount
-#sudo cp -v build/u-boot/u-boot.img image/mount
-#sudo umount image/mount
-#sudo losetup -D
+#mkfs.vfat ${BOOTFS}
 #
-#sudo losetup -o $((START2*512)) ${LOOP} ${IMG}
-#sudo mkfs.ext4 -Lrootfs ${LOOP}
-#sudo mount ${LOOP} image/mount
-#sudo rsync -av target-root/ image/mount
-#sudo umount image/mount
-#sudo losetup -D
+#mkfs.ext4 ${ROOTFS}
+#
+#
+#dd if=${BOOTFS} of=${IMG} seek=${BOOTFS_POS_SECT}
+#dd if=${ROOTFS} of=${IMG} seek=${ROOTFS_POS_SECT}
+#cat<<EOF | sfdisk ${IMG}
+#boot :   start=${BOOTFS_POS_SECT} size=      ${BOOTFS_SIZE_MB}MiB, type=b, bootable
+#rootfs : start=${ROOTFS_POS_SECT} size=      ${ROOTFS_SIZE_MB}MiB, type=83
+#EOF
+
+echo "------------------- $(pwd)"
+LOOP_DEV=$(losetup -f)
+sudo losetup -f -P ${IMG}
+sudo mount ${LOOP_DEV}p1 image/boot-fs
+sudo mount ${LOOP_DEV}p2 image/root-fs
+
+sudo cp -v build/kernel/arch/arm/boot/{zImage,dts/am335x-boneblack-wireless.dtb} image/boot-fs
+sudo cp -v build/u-boot/{MLO,u-boot.img} image/boot-fs
+sudo rsync -av target-root/ image/root-fs
+sync
+sudo umount image/boot-fs/
+sudo umount image/root-fs/
+sudo losetup --detach ${LOOP_DEV}
+
