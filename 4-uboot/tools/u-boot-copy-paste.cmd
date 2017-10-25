dfa409cd9d876a29ebe1a9ded2285674a16420cb
H.Buchmann <hans.buchmann@fhnw.ch>
2017-10-25 15:37:15 +0200

    manual booting

--- 4-uboot/tools/u-boot-copy-paste.cmd
+++ 4-uboot/tools/u-boot-copy-paste.cmd
@@ -2,3 +2,6 @@ fatls mmc 0
 fatload
 setenv xtest 'f1db7446-0dd8-499e-a34b-cc7fba3f6430'
 
+setenv kernel 'fatload mmc 0 0x82000000 zimage;fatload mmc 0 0x88000000 m335x-boneblack-wireless.dtb'
+setenv bootargs 'root=/dev/mmcblk0p2 rw rootdelay=1 init=linuxrc console=ttyO0,115200n8'
+setenv bootcmd 'run kernel;bootz 0x82000000 -  0x88000000'
