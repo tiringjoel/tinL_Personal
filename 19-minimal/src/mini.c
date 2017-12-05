1897bdc05015845e3bed14290e4f710eaa7419dd
H.Buchmann <hans.buchmann@fhnw.ch>
2017-12-05 14:06:51 +0100

    Minimal system

---
+++ 19-minimal/src/mini.c
@@ -0,0 +1,16 @@
+/*---------------------------
+  mini.c
+  (c) H.Buchmann FHNW 2017
+  ---------------------------*/
+
+#include <stdio.h> 
+
+int main(int argc,char** args)
+{
+ while(1)
+ {
+  printf("Hello World" __FILE__ "\n");
+ }
+ return 0;
+}
+
