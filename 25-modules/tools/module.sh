1c9be49195d3fc78d0083b8525bcc87cc0977879
H.Buchmann <hans.buchmann@fhnw.ch>
2018-01-10 10:09:01 +0100

    25-modules/  tools/make-it.sh -> tools/module.sh

--- 25-modules/tools/module.sh
+++ 25-modules/tools/module.sh
@@ -1,7 +1,6 @@
 #-------------------------
-#make-it.sh
+#module.sh
 #(c) H.Buchmann FHNW 2012
-#$Id$
 #see http://www.tldp.org/LDP/lkmpg/2.6/html/x181.html
 #-------------------------
 #before switching from host to target or vice versa
@@ -11,7 +10,7 @@
 #export KERNEL=/lib/modules/$(uname -r)/build
 #make -j $(getconf _NPROCESSORS_ONLN) -C ${KERNEL} V=1 M=${PWD}/src ${@}
 
-###---------------------- for target system
+#---------------------- for target system
 PREFIX=arm-linux-gnueabihf
 export KERNEL=${PWD}/../17-build/build/kernel
 make  -j $(getconf _NPROCESSORS_ONLN) -C ${KERNEL} \
