3ed723d2162cf1a5ef91cb5e2e2c21643b62e2b3
H.Buchmann <hans.buchmann@fhnw.ch>
2018-04-10 09:59:29 +0200

    10.April 2018  ziele.txt

--- 25-modules/tools/module.sh
+++ 25-modules/tools/module.sh
@@ -2,6 +2,8 @@
 #module.sh
 #(c) H.Buchmann FHNW 2012
 #see http://www.tldp.org/LDP/lkmpg/2.6/html/x181.html
+# TODO
+#   separate host system from target system
 #-------------------------
 #before switching from host to target or vice versa
 # make clean
@@ -14,9 +16,10 @@
 #---------------------- for target system
 PREFIX=arm-linux-gnueabihf
 #export KERNEL=${PWD}/kernel-devel
-export KERNEL=${PWD}/../18-resume/kernel-devel/
+DOCKER=${HOME}/devel/docker/
+export KERNEL=${DOCKER}/kernel-devel
 make  -j $(getconf _NPROCESSORS_ONLN) -C ${KERNEL} \
      ARCH=arm \
      V=0 \
-     CROSS_COMPILE=${PWD}/tc/bin/${PREFIX}- \
+     CROSS_COMPILE=${DOCKER}/tc/bin/${PREFIX}- \
      M=${PWD}/src $@
