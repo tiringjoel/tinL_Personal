12ab85b7aa21c70a04482c3da636eb500a5b2802
H.Buchmann <hans.buchmann@fhnw.ch>
2018-05-08 11:42:38 +0200

    25-hw

--- 25-modules/tools/module.sh
+++ 25-modules/tools/module.sh
@@ -8,19 +8,19 @@
 #before switching from host to target or vice versa
 # make clean
 
-##---------------------- for host system
-#export KERNEL=/lib/modules/$(uname -r)/build
-#make -j $(getconf _NPROCESSORS_ONLN) \
-#-C ${KERNEL} V=1 M=${PWD}/src ${@}
+#---------------------- for host system
+export KERNEL=/lib/modules/$(uname -r)/build
+make -j $(getconf _NPROCESSORS_ONLN) \
+-C ${KERNEL} V=0 M=${PWD}/src ${@}
 
-#---------------------- for target system
-PREFIX=arm-linux-gnueabihf
-export KERNEL=${PWD}/../18-resume/build/kernel/
-#DOCKER=${HOME}/devel/docker/
-#export KERNEL=${DOCKER}/kernel-devel
-
-make  -j $(getconf _NPROCESSORS_ONLN) -C ${KERNEL} \
-     ARCH=arm \
-     V=0 \
-     CROSS_COMPILE=${PWD}/tc/bin/${PREFIX}- \
-     M=${PWD}/src $@
+##---------------------- for target system
+#PREFIX=arm-linux-gnueabihf
+#export KERNEL=${PWD}/../18-resume/build/kernel/
+##DOCKER=${HOME}/devel/docker/
+##export KERNEL=${DOCKER}/kernel-devel
+#
+#make  -j $(getconf _NPROCESSORS_ONLN) -C ${KERNEL} \
+#     ARCH=arm \
+#     V=1 \
+#     CROSS_COMPILE=${PWD}/tc/bin/${PREFIX}- \
+#     M=${PWD}/src $@
---
+++ 26-hw/tools/module.sh
@@ -0,0 +1,23 @@
+#-------------------------
+#module.sh
+#(c) H.Buchmann FHNW 2012
+#see http://www.tldp.org/LDP/lkmpg/2.6/html/x181.html
+# TODO
+#   separate host system from target system
+#-------------------------
+#before switching from host to target or vice versa
+# make clean
+
+#---------------------- for host system
+
+#---------------------- for target system
+PREFIX=arm-linux-gnueabihf
+export KERNEL=${PWD}/../18-resume/build/kernel/
+#DOCKER=${HOME}/devel/docker/
+#export KERNEL=${DOCKER}/kernel-devel
+
+make  -j $(getconf _NPROCESSORS_ONLN) -C ${KERNEL} \
+     ARCH=arm \
+     V=0 \
+     CROSS_COMPILE=${PWD}/tc/bin/${PREFIX}- \
+     M=${PWD}/src $@
