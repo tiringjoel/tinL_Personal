1478988e145a36aa795770bb886b71120a8266c7
H.Buchmann <hans.buchmann@fhnw.ch>
2018-05-15 11:16:07 +0200

    the exercise

--- 26-hw/src/gpio-2.c
+++ 26-hw/src/gpio-2.c
@@ -67,12 +67,6 @@ struct kobj_attribute swi=
 };
 
 
-static  irqreturn_t onSWI(int id,void* d)
-{
- printk("onSWI\n");
- gpio_set_value(pin49,gpio_get_value(pin115));
- return IRQ_HANDLED;
-}
 
 static int __init _init_(void) 
 {
@@ -81,10 +75,6 @@ static int __init _init_(void)
  printk("gpio_request pin=%d res=%d\n",pin49,res);
  res=gpio_direction_output(pin49,0);
  res=gpio_request(pin115,"");
- gpio_direction_input(pin115);
- irq=gpio_to_irq(pin115);
- printk("gpio_request pin=%d res=%d irq=%d\n",pin115,res,irq);
- res=request_irq(irq,onSWI,IRQF_TRIGGER_FALLING|IRQF_TRIGGER_RISING,"swi",0);
  kobj=kobject_create_and_add("my-hw",0);
 
  res=sysfs_create_file(kobj,&led.attr);
@@ -95,7 +85,6 @@ static int __init _init_(void)
 static void __exit _exit_(void) 
 {
  printk("exit: >>>gpio-2<<<\n");
- free_irq(irq,0);
  gpio_free(pin49);
  gpio_free(pin115);
  kobject_put(kobj);
