52eea75f145b2eb5d99c4bc633363d50e51f24d8
H.Buchmann <hans.buchmann@fhnw.ch>
2018-05-28 15:23:24 +0200

    exercise

--- 26-hw/src/queue.c
+++ 26-hw/src/queue.c
@@ -17,56 +17,52 @@ typedef struct Queue
  Entry* last;
 } Queue;
 
+/*
+ q
+  first=0
+  last=0
+*/
 static void init(Queue* q)
 {
- q->first=0;
- q->last=0;
 }
 
 static unsigned empty(Queue* q)
 {
- return q->first==0;
 }
 
+
+/* before
+ first                last
+  |                   |
+  d--next-->d--next-->d--next-->0
+  
+  after
+ first                          last
+  |                             |
+  d--next-->d--next-->d--next-->d-next-->0
+                                |--------------- the new entry
+*/
 static void put(Queue* q,unsigned val)
 {
- Entry* e=malloc(sizeof(Entry));
- e->data=val;
- e->next=0;
- if (q->first==0)
-    {
-     q->first=e;
-    }
-    else
-    {
-     q->last->next=e;
-    }
- q->last=e;
 }
 
+/* before
+ first                last
+  |                   |
+  d--next-->d--next-->d--next-->0
+  
+  after
+           first      last
+            |         |
+            d--next-->d--next-->0
+*/
 static unsigned get(Queue* q)
 {
- Entry* e=q->first;
- if (e==0)
-    {
-     printf("----------- empty\n");
-     exit(1);
-    }
- q->first=e->next;
- unsigned d=e->data;
- free(e);
- return d;
 }
 
 
 static void show(Queue* q)
 {
- Entry* e=q->first;
- while(e)
- {
-  printf("%d\n",e->data);
-  e=e->next;
- }   
 }
 
 int main(int argc,char** args)
