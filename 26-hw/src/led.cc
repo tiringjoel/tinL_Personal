7736095bfb1f6579588ade0d614cc03ea5908dea
H.Buchmann <hans.buchmann@fhnw.ch>
2018-06-12 11:02:25 +0200

    the exercise

--- 26-hw/src/led.cc
+++ 26-hw/src/led.cc
@@ -5,7 +5,8 @@
 #include <fstream>
 int main(int argc,char** args)
 {
- std::ofstream led("/sys/my-hw/led"); //open 
-                                      //TODO check if file exists
- return 0;                            //destructor called close 
+ //instance
+ // check if file exists
+ return 0;
+ //destructor calls close 
 }
