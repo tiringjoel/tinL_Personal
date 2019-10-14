9c79fd7050b4f2a3357dccab3f6000a85cd73920
Hans Buchmann <hans.buchmann@fhnw.ch>
2019-10-14 14:23:17 +0200

    6-crossdevelopment

---
+++ 6-crossdevelopment/src/hello-world.cc
@@ -0,0 +1,11 @@
+//-------------------------
+//hello-world.cc
+//(c) H.Buchmann FHNW 2019
+//-------------------------
+#include <iostream>
+
+int main(int argc,char** args)
+{
+ std::cout<<"Hello--World\n";
+ return 0;
+}
