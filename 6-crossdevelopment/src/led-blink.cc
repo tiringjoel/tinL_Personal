5526d51784741e84feee93e62b0daeb7c4f94a5f
Hans Buchmann <hans.buchmann@fhnw.ch>
2019-10-22 14:31:05 +0200

    works with flush too

--- 6-crossdevelopment/src/led-blink.cc
+++ 6-crossdevelopment/src/led-blink.cc
@@ -10,6 +10,7 @@
 
 static const auto PIN="53";
 static const std::string VALUE="/sys/class/gpio/gpio53/value";
+
 void set(unsigned val)
 {
  std::ofstream value(VALUE);
@@ -19,11 +20,14 @@ void set(unsigned val)
 int main(int argc,char** args)
 {
  const auto dur=std::chrono::milliseconds(500);
+ std::ofstream value(VALUE);
  while(true)
  {
-  set(1);
+  value<<"1\n";
+  value.flush();
   std::this_thread::sleep_for(dur);
-  set(0);
+  value<<"0\n";
+  value.flush();
   std::this_thread::sleep_for(dur);
  }
  return 0;
