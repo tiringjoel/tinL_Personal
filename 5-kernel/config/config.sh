416ee303cffb97bd3089c61b5f9914d0e830bc7f
tiringjoel <joel.wirz@students.fhnw.ch>
2020-03-20 09:55:25 +0100

    added usb and wifi driver to kernel config

--- 5-kernel/config/config.sh
+++ 5-kernel/config/config.sh
@@ -5,11 +5,11 @@
 #---------------------
 #we are in 5-kernel
 PREFIX=arm-linux-gnueabihf-
-TC=${PWD}/tc/bin/
+TC=${PWD}/tc/arm-linux-gnueabihf/bin/
 CROSS_COMPILE=${TC}/${PREFIX}
 
 #----------------------- adapt for your needs
-LINUX_SRC=${HOME}/resources/beaglebone/linux
+LINUX_SRC=${HOME}/Desktop/tinL1/tinL_own/resources/linux
 BUILD=$(pwd)/build
 MODULES=$(pwd)/modules
 
