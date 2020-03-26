b24de4485b7843b0a31537f91001727957c727d2
Hans Buchmann <hans.buchmann@fhnw.ch>
2020-03-26 16:38:18 +0100

    more comment

--- 6-http/src/gpio.sh
+++ 6-http/src/gpio.sh
@@ -7,10 +7,14 @@
 #    disable LEDS_GPIO in kernel
 #    new kernel zImage see 5-kernel
 #-----------------------
-PIN=53 #32+21 LED USR0
+PIN=53 #32+21 LED USR0 BBB_SRM GPIO1_21
+# GPIO_0:  0 .. 31  32 pins
+# GPIO_1: 32 .. 63  32 pins
+# GPIO_2: 64 .. 95  32 pins
+
 # enter /sys/class/gpio
-# create gpio53
-# direction of gpio53 out
-# write 1 > gpio53/value
-#       0> gpio53/value
+# create gpio53:            echo 53  > export
+# direction of gpio53 out   echo out > gpio53/direction
+# write 1 > gpio53/value    echo 1 > gpio53/value
+#       0> gpio53/value     echo 0 > gpio53/value
 
