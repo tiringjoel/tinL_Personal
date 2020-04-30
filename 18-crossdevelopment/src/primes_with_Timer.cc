aa7534dc09335205bccffd70ffe4765b944a49b4
tiringjoel <joel.wirz@students.fhnw.ch>
2020-04-30 17:26:29 +0200

    added primes_with_timer.cc in 18-crossdevelopment

---
+++ 18-crossdevelopment/src/primes_with_Timer.cc
@@ -0,0 +1,62 @@
+//-------------------------
+//primes as benchmark
+//(c) H.Buchmann FHNW 2018
+//-------------------------
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+class Primes
+{
+ public:
+  Primes(unsigned n); //primes<=n
+
+ private:
+  std::vector<unsigned> primes;
+  bool isPrime(unsigned v); 
+};
+
+Primes::Primes(unsigned n)
+{
+ std::cout<<"start\n";
+ primes.push_back(2);
+ for(unsigned z=2;z<=n;++z)
+ {
+  if (isPrime(z)) 
+     {
+      primes.push_back(z);
+     }
+ }
+#if 0
+ for(auto p:primes)
+ {
+  std::cout<<p<<"\n";
+ }
+#endif
+ std::cout<<"done\n";
+}
+
+bool Primes::isPrime(unsigned z)
+{
+ unsigned i=0;
+ while(true)
+ {
+  auto p=primes[i];
+// z=q*p + r  
+  auto r=z%p;
+  if (r==0) return false;
+  auto q=z/p;
+  if (q<=p) return true;
+  ++i;
+ }
+}
+
+int main(int argc,char** args)
+{
+ auto t1 = std::chrono::high_resolution_clock::now();
+ Primes primes(1'000'000); //a lot of primes
+ auto t2 = std::chrono::high_resolution_clock::now();
+ auto duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+ std::cout << duration;
+ return 0;
+}
