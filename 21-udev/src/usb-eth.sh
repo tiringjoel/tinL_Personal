abd167f507fde194f244ce6dacbeeeb722f575a0
tiringjoel <joel.wirz@students.fhnw.ch>
2020-05-28 16:22:28 +0200

    adapted 21-udev/src

--- 21-udev/src/usb-eth.sh
+++ 21-udev/src/usb-eth.sh
@@ -4,11 +4,12 @@
 #(c) H.Buchmann FHNW 2018
 #--------------------------------
 echo "--------------- action: ${1} device: ${2} ID_VENDOR_ID: ${3}" >> /tmp/usb-eth.log
-case ${1} in 
- enable)
- ifconfig ${2} 192.168.7.1
- ;;
- disable)
- ifconfig ${2} down
- ;;
-esac
+
+#case ${1} in 
+# enable)
+# ifconfig ${2} 192.168.7.1
+# ;;
+# disable)
+# ifconfig ${2} down
+# ;;
+#esac
